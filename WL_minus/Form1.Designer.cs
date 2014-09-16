25f0fbb1b64454750df4cf56cbaca5f5d1cb8130
saad <saad0209@gmail.com>
2014-09-17 00:30:12 +0600

    csv generated graph not done yet

---
+++ WL_minus/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WL_minus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnSecond = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.chkHighest = new System.Windows.Forms.CheckBox();
+            this.lblyear = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(13, 12);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 0;
+            this.btnFirst.Text = "First Folder";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirstClick);
+            // 
+            // btnSecond
+            // 
+            this.btnSecond.Location = new System.Drawing.Point(94, 12);
+            this.btnSecond.Name = "btnSecond";
+            this.btnSecond.Size = new System.Drawing.Size(96, 23);
+            this.btnSecond.TabIndex = 1;
+            this.btnSecond.Text = "Second Folder";
+            this.btnSecond.UseVisualStyleBackColor = true;
+            this.btnSecond.Click += new System.EventHandler(this.btnSecondClick);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(150, 106);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate CSVs";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerateClick);
+            // 
+            // chkHighest
+            // 
+            this.chkHighest.AutoSize = true;
+            this.chkHighest.Location = new System.Drawing.Point(94, 52);
+            this.chkHighest.Name = "chkHighest";
+            this.chkHighest.Size = new System.Drawing.Size(86, 17);
+            this.chkHighest.TabIndex = 4;
+            this.chkHighest.Text = "Only Highest";
+            this.chkHighest.UseVisualStyleBackColor = true;
+            // 
+            // lblyear
+            // 
+            this.lblyear.AutoSize = true;
+            this.lblyear.Location = new System.Drawing.Point(12, 56);
+            this.lblyear.Name = "lblyear";
+            this.lblyear.Size = new System.Drawing.Size(58, 13);
+            this.lblyear.TabIndex = 5;
+            this.lblyear.Text = "Year name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 141);
+            this.Controls.Add(this.lblyear);
+            this.Controls.Add(this.chkHighest);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSecond);
+            this.Controls.Add(this.btnFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnSecond;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.CheckBox chkHighest;
+        private System.Windows.Forms.Label lblyear;
+    }
+}
+
---
+++ WL_minus/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WL_minus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string pathseperator = "\\";
+        string FirstFilesEntries = "C:\\Users\\Gun2sh\\Desktop\\storm surge\\data\\Excess_WL\\2047\\", SecondFilesEntries = "C:\\Users\\Gun2sh\\Desktop\\Database\\WL\\2047";
+        private void btnFirstClick(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                FirstFilesEntries = folderBrowserDialog1.SelectedPath;
+                //FirstFilesEntries = Directory.GetFiles(path);
+                 
+            }
+        }
+
+        private void btnSecondClick(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                 SecondFilesEntries = folderBrowserDialog1.SelectedPath;
+                //SecondFilesEntries = Directory.GetFiles(path);
+                 
+            }
+        }
+        public Dictionary<DateTime, double> getDataFromCSV(StreamReader reader)
+        {
+
+            Dictionary<DateTime, double> obs1 = new Dictionary<DateTime, double>();
+            int i = 0;
+            while (!reader.EndOfStream)
+            {
+
+                string line = reader.ReadLine();
+                i++;
+                if (i == 1) continue;
+
+                string[] values = line.Split(',');
+
+                DateTime tmpDate = DateTime.Parse(values[0]).Date;
+                if (!obs1.ContainsKey(tmpDate))
+                    obs1.Add(tmpDate, double.Parse(values[1]));
+                else
+                {
+                    if (obs1[tmpDate] < double.Parse(values[1]))
+                        obs1[tmpDate] = double.Parse(values[1]);
+                }
+
+
+            }
+            return obs1;
+        }
+        private void btnGenerateClick(object sender, EventArgs e)
+        {
+            String savedir = "C:\\Users\\Gun2sh\\Desktop\\Camera";
+            Dictionary<DateTime, double> resultsMap = new Dictionary<DateTime, double>();
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                savedir = folderBrowserDialog1.SelectedPath;
+            }
+            try
+            {
+                
+                string yearname = Path.GetFileName(Path.GetDirectoryName(FirstFilesEntries+pathseperator));
+                if (Directory.Exists(savedir + pathseperator + yearname + pathseperator))
+                {
+                    Directory.Delete(savedir + pathseperator + yearname + pathseperator, true);
+                }
+                Directory.CreateDirectory(savedir + pathseperator + yearname + pathseperator);
+                foreach (string stormName in Directory.GetDirectories(FirstFilesEntries))
+                {
+                    string strmname = Path.GetFileName(Path.GetDirectoryName(stormName + pathseperator));
+                    if (Directory.Exists(savedir + pathseperator + yearname + pathseperator + strmname + pathseperator))
+                    {
+                        Directory.Delete(savedir + pathseperator + yearname + pathseperator + strmname + pathseperator, true);
+                    }
+                    Directory.CreateDirectory(savedir + pathseperator + yearname + pathseperator + strmname + pathseperator);
+                    foreach (string firstDir in Directory.GetFiles(stormName))
+                    {
+                        //
+                        StreamReader reader = new StreamReader(File.OpenRead(firstDir));
+                        Dictionary<DateTime, double> obs1 = getDataFromCSV(reader);
+                        lblyear.Text = obs1.Keys.ToList()[0].Year.ToString();
+                        string filename = Path.GetFileName(firstDir);
+                        if (File.Exists(SecondFilesEntries + pathseperator + filename))
+                        {
+                            StringBuilder csv = new StringBuilder();
+                            csv.Append(String.Format("{0},{1}{2}", "date and time", "water level (m)", Environment.NewLine));
+                            reader = new StreamReader(File.OpenRead(SecondFilesEntries + pathseperator + filename));
+                            Dictionary<DateTime, double> obs2 = getDataFromCSV(reader);
+                            foreach (DateTime dt in obs1.Keys)
+                            {
+                                double dtemp = obs1[dt] - obs2[dt];
+                                if (!resultsMap.ContainsKey(dt))
+                                    resultsMap.Add(dt, dtemp);
+                                else
+                                {
+                                    if (resultsMap[dt] < dtemp)
+                                        resultsMap[dt] = dtemp;
+                                }
+                            }
+                            DateTime forhighestDT = DateTime.Now;
+                            double val = 0.0;
+                            foreach (DateTime dt in resultsMap.Keys)
+                            {
+                                if (chkHighest.Checked)
+                                {
+                                    if (val < resultsMap[dt]) {
+                                        forhighestDT = dt;
+                                        val = resultsMap[dt];
+                                    }
+                                }
+                                else
+                                    csv.Append(String.Format("{0},{1}{2}", dt.ToString("yyyy-MM-dd"), resultsMap[dt].ToString(), Environment.NewLine));
+                            }
+                            if (chkHighest.Checked) {
+                                csv.Append(String.Format("{0},{1}{2}", forhighestDT.ToString("yyyy-MM-dd"), val.ToString(), Environment.NewLine));
+                            }
+
+
+                            File.WriteAllText(savedir + pathseperator + yearname + pathseperator +
+                                strmname + pathseperator + filename, csv.ToString());
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
